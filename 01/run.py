c56dc45355fb786d83ecd69cc632591150b08585
Mikołaj Aleksandrzak <37934103+miccowhy@users.noreply.github.com>
2019-12-01 12:18:13 +0100

    01.12 tasks

---
+++ 01/run.py
@@ -0,0 +1,31 @@
+import math
+
+fuel_count = lambda x: math.floor(x / 3) - 2
+
+
+def calculate_fuel_recursion(atomic_mass):
+    atomic_mass = fuel_count(atomic_mass)
+    if atomic_mass <= 0:
+        return 0
+    else:
+        return atomic_mass + calculate_fuel_recursion(atomic_mass)
+
+
+def calculate_fuel_requirement():
+    module_mass_fuel = 0
+    whole_fuel = 0
+    with open("input", "r") as file_in:
+        for mass in file_in:
+            module_mass_fuel += fuel_count(int(mass))
+            whole_fuel += calculate_fuel_recursion(int(mass))
+
+    return module_mass_fuel, whole_fuel
+
+
+if __name__ == "__main__":
+    answer = calculate_fuel_requirement()
+
+    print(f"""
+    Module fuel requirement: {answer[0]}
+    Full fuel requirement: {answer[1]} 
+    """)
