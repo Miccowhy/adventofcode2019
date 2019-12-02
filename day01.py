7eca93991b876695fd1db434f3ca3555bdba04e4
Miccowhy <kryzystworczy@gmail.com>
2019-12-02 15:44:50 +0100

    rewrite with list comprehension

--- day01.py
+++ day01.py
@@ -12,12 +12,9 @@ def calculate_fuel_recursion(atomic_mass):
 
 
 def calculate_fuel_requirement():
-    module_mass_fuel = 0
-    whole_fuel = 0
-    with open("input/01", "r") as file_in:
-        for mass in file_in:
-            module_mass_fuel += fuel_count(int(mass))
-            whole_fuel += calculate_fuel_recursion(int(mass))
+    mass = [int(element) for element in open("input/01").read().splitlines()]
+    module_mass_fuel = sum([fuel_count(element) for element in mass])
+    whole_fuel = sum([calculate_fuel_recursion(element) for element in mass])
 
     return module_mass_fuel, whole_fuel
 
@@ -27,5 +24,6 @@ if __name__ == "__main__":
 
     print(f"""
     Module fuel requirement: {answer[0]}
-    Full fuel requirement: {answer[1]} 
+    Full fuel requirement: {answer[1]}
     """)
+
