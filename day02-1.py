58ae5e016833c1e0f5079c458b39a88d2758dd9a
Miccowhy <mikolaj.d.aleksandrzak@gmail.com>
2019-12-02 09:13:14 +0100

    add value change at pos 1&2

--- day02-1.py
+++ day02-1.py
@@ -6,6 +6,9 @@ with open("src/02", "r") as input:
 i = 0
 operator = {1: operator.add,
 2: operator.mult}
+list[1] = 12
+list[2] = 2
+
 while list[i:= i+4] != 99:
     list[list[i+3]] = operator[list[i]](list[list[i+1]], list[list[i+2])
 
