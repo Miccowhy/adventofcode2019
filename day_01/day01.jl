80b728063a601aa58b23f82ed2a1307f03f5e779
Miccowhy <kryzystworczy@gmail.com>
2019-12-03 00:26:51 +0100

    Label output, as in Python solution

--- day_01/day01.jl
+++ day_01/day01.jl
@@ -6,5 +6,5 @@ data = parse.(Int,readlines("input"))
 fuel_count(mass) = mass ÷ 3 - 2
 recursive_fuel(mass) = mass > 0 ? mass + recursive_fuel(fuel_count(mass)) : 0
 
-println(sum(fuel_count.(data)))
-println(sum(recursive_fuel.(fuel_count.(data))))
+println("Module fuel requirement: $(sum(fuel_count.(data)))")
+println("Full fuel requirement: $(sum(recursive_fuel.(fuel_count.(data))))")
