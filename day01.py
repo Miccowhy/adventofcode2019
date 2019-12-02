7dc3307f67245fc1cdec834980aeadf50b3f5d21
Mikołaj Aleksandrzak <37934103+miccowhy@users.noreply.github.com>
2019-12-02 23:53:24 +0100

    Shorten code, based on Julia solution

--- day01.py
+++ day01.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-
 # Advent of Code 2019
 # Day 1
 
 fuel_count = lambda x: x // 3 - 2
+mass = [int(element) for element in open("input/01").read().splitlines()]
 
 
-def calculate_fuel_recursion(atomic_mass):
-    atomic_mass = fuel_count(atomic_mass)
-    return atomic_mass + calculate_fuel_recursion(atomic_mass) if atomic_mass > 0 else 0
-
-
-def calculate_fuel_requirement():
-    mass = [int(element) for element in open("input/01").read().splitlines()]
-    module_mass_fuel = sum([fuel_count(element) for element in mass])
-    whole_fuel = sum([calculate_fuel_recursion(element) for element in mass])
-
-    return module_mass_fuel, whole_fuel
+def calculate_fuel_recursion(atomic_mass: int):
+    return atomic_mass + calculate_fuel_recursion(fuel_count(atomic_mass)) if atomic_mass > 0 else 0
 
 
 if __name__ == "__main__":
-    answer = calculate_fuel_requirement()
-
     print(f"""
-    Module fuel requirement: {answer[0]}
-    Full fuel requirement: {answer[1]}
+    Module fuel requirement: {sum([fuel_count(element) for element in mass])}
+    Full fuel requirement: {sum([calculate_fuel_recursion(fuel_count(element)) for element in mass])}
     """)
-
