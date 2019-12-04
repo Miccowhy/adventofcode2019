15704a48d8825e4728368145500db803d4784f43
Miccowhy <kryzystworczy@gmail.com>
2019-12-04 12:52:14 +0100

    Solution to task 4.1

---
+++ day_04/day04-1.jl
@@ -0,0 +1,4 @@
+doubledigit(password) = true in (password[i] == password[i+1] for i in 1:5)
+ascending(password) = !(false in (password[i] <= password[i+1] for i in 1:5))
+
+sum(doubledigit(string(password)) && ascending(string(password)) for password in 146810:612564)
