0ef1c3d136f6eadefe38decdb5aba10276e9a647
Mikołaj Aleksandrzak <37934103+miccowhy@users.noreply.github.com>
2019-12-04 20:03:46 +0100

    fix indents

--- day_04/day04-2.jl
+++ day_04/day04-2.jl
@@ -5,19 +5,19 @@
 ascending(password) = !(false in (password[i] <= password[i+1] for i in 1:5))
 
 function naive_doubles(password)
-        seq = [0]
-        let i = 1, j = 1
-                while i <= 6 && j <= 6
-                        if password[i] == password[j]
-                                seq[end] += 1
-                                j += 1
-                        else
-                                append!(seq, 0)
-                                i = j
-                        end
-                end
+    seq = [0]
+    let i = 1, j = 1
+        while i <= 6 && j <= 6
+            if password[i] == password[j]
+                seq[end] += 1
+                j += 1
+            else
+                append!(seq, 0)
+                i = j
+            end
         end
-        2 in seq
+    end
+    2 in seq
 end
 
 println(sum(naive_doubles(string(password)) && ascending(string(password)) for password in 146810:612564))
