a1d7616bc8f1a193d430a88a83cf584985fe9200
Mikołaj Aleksandrzak <37934103+miccowhy@users.noreply.github.com>
2019-12-06 22:44:32 +0100

    task 6.2 and refactor of 6.1 boilerplate

--- day_06/day06.py
+++ day_06/day06.py
@@ -7,42 +7,39 @@ data = open("input").read()
 
 
 def compute_orbits(center):
-    if center in orbits:
-        for planet in orbits[center]:
-            return 1 + compute_orbits(planet)
-    else:
-        return 0
+    return 1 + compute_orbits(orbits[center]) if center in orbits else 0
 
 
 def set_orbits(data):
     for line in data.splitlines():
         line = line.split(")")
-        if line[1] not in orbits:
-            orbits[line[1]] = {line[0]}
-        else:
-            orbits[line[1]].add(line[0])
+        orbits[line[1]] = line[0]
 
 
-def task1_run():
+def sum_all_orbits():  # Task 1
     return sum(compute_orbits(center) for center in orbits)
 
 
-# TODO: fix path returning None
-def path_to_root(node, path):
-    path.append(node)
-    print(path)
+def path_to_root(node, path=None):
+    if path is None:
+        path = list()
     if node in orbits:
-        [path_to_root(next_node, path) for next_node in orbits[node]]
-    else:  # node == "COM":
-        return path
+        path.append(node)
+        path_to_root(orbits[node], path)
+    return path
+
+
+def compute_distance_between():  # Task 2
+    first_path = path_to_root(orbits["YOU"])
+    second_path = path_to_root(orbits["SAN"])
+
+    for i in range(len(first_path)):
+        if first_path[i] in set(second_path):
+            return i + second_path.index(first_path[i])
 
 
 if __name__ == "__main__":
     set_orbits(data)
-    print('COM' in orbits)
-    print('B' in orbits)
-    for key in orbits:
-        print(key)
-    #print(task1_run())
-    #print(path_to_root("YOU"))
-    print(path_to_root("C", []))
+
+    print(sum_all_orbits())
+    print(compute_distance_between())
