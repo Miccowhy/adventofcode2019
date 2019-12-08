b8d452f323be7cd50f01b9057c72eb439c523490
Mikołaj Aleksandrzak <37934103+miccowhy@users.noreply.github.com>
2019-12-08 19:10:59 +0100

    add tests, refactor day 5,

--- day_05/day05.py
+++ day_05/day05.py
@@ -2,86 +2,34 @@
 # Advent of Code 2019
 # Day 5
 
-import operator
 from collections import deque
 
 task_input = open("input").read().rstrip().split(",")
 task_input = [int(element) for element in task_input]
 
-#task_input = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
-#              1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
-#              999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
 
-def write_input(value, *_):
-    data[value] = input()  # 1: 1, 2: 5
-
-
-def output_parameter(value, *_):
-    print(data[value])
-
-
-instruction = {1: operator.add,
-               2: operator.mul,
-               3: write_input,
-               4: output_parameter}
-
-
-def opcode_expansion(value):
-    opcode = deque(str(value))
-    if len(opcode) < 5:
-        for i in range(len(opcode), 5):
-            opcode.appendleft('0')
-    return opcode
-
-
-def intcode(data):
+def intcode(data, *args):
+    arguments = deque(args)
     i = 0
     while data[i] != 99 and i < len(data):
-        opcode = opcode_expansion(data[i])
-        c, b, a = [int(i + 3 - digit) if int(opcode[digit]) else int(data[i + 3 - digit]) for digit in range(3)]
+        try:
+            a, b, c = [i + x if data[i] // 10**(x+1) % 10 else data[i + x] for x in range(1, 4)]
+        except IndexError:
+            pass
 
-        if opcode[-1] == '1':
-            data[c] = instruction[1](int(data[a]), int(data[b]))
-            if c != i:
-                i += 4
-        elif opcode[-1] == '2':
-            data[c] = instruction[2](int(data[a]), int(data[b]))
-            if c != i:
-                i += 4
-        elif opcode[-1] == '3':
-            instruction[3](a)
-            if a != i:
-                i += 2
-        elif opcode[-1] == '4':
-            output_parameter(a)
-            i += 2
-
-        elif opcode[-1] == '5':
-            if data[a]:
-                i = data[b]
-            else:
-                i += 3
-        elif opcode[-1] == '6':
-            if not int(data[a]):
-                i = int(data[b])
-            else:
-                i += 3
-        elif opcode[-1] == '7':
-            data[c] = 1 if int(data[a]) < int(data[b]) else 0
-            if c != i:
-                i += 4
-        elif opcode[-1] == '8':
-            data[c] = 1 if int(data[a]) == int(data[b]) else 0
-            if c != i:
-                i += 4
-
-        else:
-            print(f"fuck: {i}")
-            break
+        opcode = data[i] % 100
+        if opcode == 99: break
+        elif opcode == 1: data[c] = data[a] + data[b]; i += 4
+        elif opcode == 2: data[c] = data[a] * data[b]; i += 4
+        elif opcode == 3: data[a] = arguments.popleft(); i += 2  #
+        elif opcode == 4: print(data[a]); i += 2
+        elif opcode == 5: i = data[b] if data[a] else i + 3
+        elif opcode == 6: i = data[b] if not data[a] else i + 3
+        elif opcode == 7: data[c] = 1 if data[a] < data[b] else 0; i = i + 4
+        elif opcode == 8: data[c] = 1 if data[a] == data[b] else 0; i = i + 4
 
 
 if __name__ == "__main__":
-    global data
-    data = task_input.copy()
+    intcode(task_input.copy(), 1)  # Task 1
 
-    intcode(data)
+    intcode(task_input.copy(), 5)  # Task 2
--- day_05/test_day05.py
+++ day_05/test_day05.py
@@ -1,13 +1,124 @@
-import unittest
+from unittest.mock import patch
+from unittest import TestCase
+import io
+import sys
 
 from day_05 import day05
 
 
-class Task1Test(unittest.TestCase):
-    def setUp(self):
-        self.data = [1101, 100, -1, 4, 0]
+class Task1Test(TestCase):
+    def test_output_passed_input(self):
+        data = [3, 0, 4, 0, 99]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        day05.intcode(data, 7)
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(7, int(captured_output.getvalue()))
 
     def test_task1(self):
-        day05.intcode(self.data)
-        self.assertEqual(99, self.data[4])
+        data = [1101, 100, -1, 4, 0]
+        day05.intcode(data)
+        self.assertEqual(99, data[4])
 
+
+class Task2Test(TestCase):
+    def test_equal_8_position(self):
+        data = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        day05.intcode(data.copy(), 7)
+        day05.intcode(data.copy(), 8)
+        day05.intcode(data.copy(), 9)
+        sys.stdout = sys.__stdout__
+
+        return_value = [int(value) for value in captured_output.getvalue().splitlines()]
+        self.assertEqual(0, return_value[0])
+        self.assertEqual(1, return_value[1])
+        self.assertEqual(0, return_value[2])
+
+    def test_less_than_8_position(self):
+        data = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        day05.intcode(data.copy(), 7)
+        day05.intcode(data.copy(), 8)
+        day05.intcode(data.copy(), 9)
+        sys.stdout = sys.__stdout__
+
+        return_value = [int(value) for value in captured_output.getvalue().splitlines()]
+        self.assertEqual(1, return_value[0])
+        self.assertEqual(0, return_value[1])
+        self.assertEqual(0, return_value[2])
+
+    def test_equal_8_immediate(self):
+        data = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        day05.intcode(data.copy(), 7)
+        day05.intcode(data.copy(), 8)
+        day05.intcode(data.copy(), 9)
+        sys.stdout = sys.__stdout__
+
+        return_value = [int(value) for value in captured_output.getvalue().splitlines()]
+        self.assertEqual(0, return_value[0])
+        self.assertEqual(1, return_value[1])
+        self.assertEqual(0, return_value[2])
+
+    def test_less_than_8_immediate(self):
+        data = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        day05.intcode(data.copy(), 7)
+        day05.intcode(data.copy(), 8)
+        day05.intcode(data.copy(), 9)
+        sys.stdout = sys.__stdout__
+
+        return_value = [int(value) for value in captured_output.getvalue().splitlines()]
+        self.assertEqual(1, return_value[0])
+        self.assertEqual(0, return_value[1])
+        self.assertEqual(0, return_value[2])
+
+    def test_jump_position(self):
+        data = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        day05.intcode(data.copy(), -1)
+        day05.intcode(data.copy(), 0)
+        day05.intcode(data.copy(), 1)
+        sys.stdout = sys.__stdout__
+
+        return_value = [int(value) for value in captured_output.getvalue().splitlines()]
+        self.assertEqual(1, return_value[0])
+        self.assertEqual(0, return_value[1])
+        self.assertEqual(1, return_value[2])
+
+    def test_jump_immediate(self):
+        data = [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        day05.intcode(data.copy(), -1)
+        day05.intcode(data.copy(), 0)
+        day05.intcode(data.copy(), 1)
+        sys.stdout = sys.__stdout__
+
+        return_value = [int(value) for value in captured_output.getvalue().splitlines()]
+        self.assertEqual(1, return_value[0])
+        self.assertEqual(0, return_value[1])
+        self.assertEqual(1, return_value[2])
+
+    def test_long_equal_8(self):
+        data = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        day05.intcode(data.copy(), 7)
+        day05.intcode(data.copy(), 8)
+        day05.intcode(data.copy(), 9)
+        sys.stdout = sys.__stdout__
+
+        return_value = [int(value) for value in captured_output.getvalue().splitlines()]
+        self.assertEqual(999, return_value[0])
+        self.assertEqual(1000, return_value[1])
+        self.assertEqual(1001, return_value[2])
